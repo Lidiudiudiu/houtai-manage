c094a8006137793456b4d2072aa03dff412f1121
Lidiudiudiu <08223241@cumt.edu.cn>
2024-07-19 15:51:02 +0800

    vue-router路由的引入

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import {
+    createRouter,
+    createWebHashHistory
+} from 'vue-router'
+
+//路由规则
+const routes = [
+
+]
+
+//创建路由
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+//暴露路由
+export default router
