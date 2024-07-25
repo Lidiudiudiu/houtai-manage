485099751d7e4c2676eee967c97fdc82d1d98066
Lidiudiudiu <08223241@cumt.edu.cn>
2024-07-25 08:53:03 +0800

    解决刷新后请求数据页面消失

--- src/permission.js
+++ src/permission.js
@@ -1,7 +1,8 @@
 import router from '~/router'
 import { getcookies } from '~/composables/auth'
 import { toast } from '~/composables/util'
-router.beforeEach((to, from, next) => {
+import store from './store'
+router.beforeEach(async (to, from, next) => {
     const token = getcookies();
     if (!token && to.path != '/login') {
         toast('请先登录', 'error')
@@ -11,6 +12,10 @@ router.beforeEach((to, from, next) => {
     if (token && to.path == '/login') {
         toast('请勿重复登录', 'error')
         return next({ path: from.path ? from.path : '/' })
+    }
+    //如果登录，请求用户信息，存储在vuex中
+    if (token) {
+        await store.dispatch('getinfo')
     }
 
     next()
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'vuex'
-
+import { getinfo } from '~/api/manager.js'
 // 创建一个新的 store 实例
 const store = createStore({
     state() {
@@ -10,6 +10,16 @@ const store = createStore({
     mutations: {
         SET_USERINFO(state, userinfo) {
             state.user = userinfo
+        }
+    },
+    actions: {
+        getinfo({ commit }) {
+            return new Promise((resolve, reject) => {
+                getinfo().then(res => {
+                    commit("SET_USERINFO", res)
+                    resolve(res)
+                }).catch(err => reject(err))
+            })
         }
     }
 })
